5bcc263829318fe9b11ab1294b6beea30435d211
tjnk24 <tjnk24@gmail.com>
2020-06-17 06:30:42 +0300

    working on modals

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,21 @@
 import React, { FC } from 'react';
+import Login from '@components/modal/login';
+
 import classnames from 'classnames/bind';
 import style from './style.scss';
 
 const cn = classnames.bind(style);
 
 const Header: FC = () => (
-  <header className={cn('header')}>
-    <h1 className={cn('header__title')}>Your airport schedule</h1>
-  </header>
+  <>
+    {/* <Login /> */}
+    <header className={cn('header')}>
+      <h1 className={cn('header__title')}>Your airport schedule</h1>
+      <div className={cn('header__buttons-wrap')}>
+        <button type="button">Login / Sign up</button>
+      </div>
+    </header>
+  </>
 );
 
 export default Header;
