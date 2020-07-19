4fc514fe9c484309c56ab4844d1a9c75690a005f
tjnk24 <tjnk24@gmail.com>
2020-07-19 22:18:24 +0300

    fixing email verify modal bug

--- src/components/modals/message/index.tsx
+++ src/components/modals/message/index.tsx
@@ -1,13 +1,13 @@
 import React, { FC } from 'react';
 import Modal from 'react-bootstrap/esm/Modal';
 
-import { EMAIL_VERIFY } from '../routes';
+import { MESSAGE } from '../routes';
 
 import ModalProps from '../types';
 
 const Message: FC<ModalProps> = ({ modal, handler }) => (
   <Modal
-    show={modal?.route === EMAIL_VERIFY}
+    show={modal?.route === MESSAGE}
     onHide={() => handler?.({ route: '' })}
     backdrop={false}
   >
